8953a7f93060ada8a5345ac7b2680ddb6e6e4a37
Erick384 <erickwachuma@gmail.com>
2023-06-08 13:50:48 +0300

    Uploading Commits

--- MultiStep_Form/src/App.jsx
+++ MultiStep_Form/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,82 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="left">
+        <div className="left1">
+          <div className="circle"><h3>1</h3></div>
+          <div className="content">
+            <span><a href="index.html">Step 1</a></span>
+            <p>YOUR INFO</p>
+          </div>
+        </div>
+        <div className="left1">
+          <div className="circle"><h3>2</h3></div>
+          <div className="content">
+            <span> <a href="./step2/step2.html">Step 2</a></span>
+            <p>SELECT PLAN</p>
+          </div>
+        </div>
+        <div className="left1">
+          <div className="circle"><h3>3</h3></div>
+          <div className="content">
+            <span><a href="./step3/step3.html">Step 3</a></span>
+            <p>ADDS-ON</p>
+          </div>
+        </div>
+        <div className="left1">
+          <div className="circle"><h3>4</h3></div>
+          <div className="content">
+            <span> <a href="./step4/step4.html">Step 4</a></span>
+            <p>SUMMARY</p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="right">
+        <div className="right-txt">
+          <div className="header">
+            <h1>Personal info</h1>
+            <br />
+            <p>Please provide your name, email address, and phone number.</p>
+          </div>
+          <div className="form">
+            <div className="inputs">
+              <label htmlFor="text">NAME</label>
+              <input type="text" placeholder="e.g.Stephen King" required />
+            </div>
+            <div className="inputs">
+              <label htmlFor="text">Email Address</label>
+              <input
+                type="email"
+                name="text"
+                id="email"
+                placeholder="e.g.stephenking@lorem.com"
+                required
+              />
+            </div>
+            <div class="inputs">
+              <label for="number">Phone Number</label>
+              <input
+                type="tel"
+                id="phonenumber"
+                placeholder="e.g. +1 243 567 890"
+                required
+              />
+            </div>
+          </div>
+        </div>
+        <div className="button">
+          <button><a href="./step2/step2.html">Next Step</a></button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
+
+    <div className="attribution">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+        >Frontend Mentor</a
+      >. Coded by <a href="#">Erick-384</a>.
+    </div>
     </>
   )
 }
