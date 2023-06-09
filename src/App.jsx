00e8caeda20a4478e008f67a5021e43c372fa6be
Erick384 <erickwachuma@gmail.com>
2023-06-09 21:47:16 +0300

    Uploading Commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import PersonInfo from './Components/PersonInfo';
 import './App.css'
 
 function App() {
@@ -38,44 +39,10 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="right">
-        <div className="right-txt">
-          <div className="header">
-            <h1>Personal info</h1>
-            <br />
-            <p>Please provide your name, email address, and phone number.</p>
-          </div>
-          <div className="form">
-            <div className="inputs">
-              <label htmlFor="text">NAME</label>
-              <input type="text" placeholder="e.g.Stephen King" required />
-            </div>
-            <div className="inputs">
-              <label htmlFor="text">Email Address</label>
-              <input
-                type="email"
-                name="text"
-                id="email"
-                placeholder="e.g.stephenking@lorem.com"
-                required
-              />
-            </div>
-            <div class="inputs">
-              <label for="number">Phone Number</label>
-              <input
-                type="tel"
-                id="phonenumber"
-                placeholder="e.g. +1 243 567 890"
-                required
-              />
-            </div>
-          </div>
-        </div>
-        <div className="button">
-          <button><a href="./step2/step2.html">Next Step</a></button>
-        </div>
-      </div>
+      
+      <PersonInfo />
     </div>
+
 
     <div className="attribution">
       Challenge by
---
+++ src/Components/PersonInfo.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import "./PersonInfo.css"
+
+function PersonInfo() {
+  return (
+    <div className="right">
+        <div className="right-txt">
+          <div className="header">
+            <h1>Personal info</h1>
+            <br />
+            <p>Please provide your name, email address, and phone number.</p>
+          </div>
+          <div className="form">
+            <div className="inputs">
+              <label htmlFor="text">NAME</label>
+              <input type="text" placeholder="e.g.Stephen King" required />
+            </div>
+            <div className="inputs">
+              <label htmlFor="text">Email Address</label>
+              <input
+                type="email"
+                name="text"
+                id="email"
+                placeholder="e.g.stephenking@lorem.com"
+                required
+              />
+            </div>
+            <div class="inputs">
+              <label for="number">Phone Number</label>
+              <input
+                type="tel"
+                id="phonenumber"
+                placeholder="e.g. +1 243 567 890"
+                required
+              />
+            </div>
+          </div>
+        </div>
+        <div className="button">
+          <button><a href="./step2/step2.html">Next Step</a></button>
+        </div>
+      </div>
+  )
+}
+
+export default PersonInfo
